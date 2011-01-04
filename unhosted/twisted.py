67bd0fe66586ea61e62ec7ad89618b2c84fbd2df
Divius <divius.inside@gmail.com>
2011-01-05 01:36:57 +0300

    unhosted.twisted: Correctly handle client failure

--- unhosted/twisted.py
+++ unhosted/twisted.py
@@ -49,6 +49,7 @@ class UnhostedResource(resource.Resource):
         """C-tor."""
         self.unhosted = unhosted
         self.d = None
+        self.canceled = False
 
     def render_POST(self, request):
         """Render POST request."""
@@ -56,6 +57,7 @@ class UnhostedResource(resource.Resource):
         self.d = defer.maybeDeferred(self.unhosted.processRequest(args))
         self.d.addCallback(self._ready, request)
         self.d.addErrback(self._error, request)
+        request.notifyFinish().addErrback(self._fail)
         return server.NOT_DONE_YET
 
     # Protected
@@ -64,16 +66,26 @@ class UnhostedResource(resource.Resource):
         """Requested data ready."""
         if not isinstance(data, str):
             data = unhosted.utils.jwrite(data)
+
         request.setResponseCode(200, "OK")
         request.write(data + "\n")
         request.finish()
 
     def _error(self, err, request):
         """Error while requesting data."""
+        if self.canceled:
+            return
+
         if isinstance(err.value, unhosted.http.HttpStatus):
             request.setResponseCode(err.value.code(), err.getErrorMessage())
         else:
             request.setResponseCode(unhosted.http.HttpInternalServerError._code,
                 "Unknown exception: " + err.getErrorMessage())
+
         request.write(err.getErrorMessage() + "\n")
         request.finish()
+
+    def _fail(self, err):
+        """Client-side error."""
+        self.canceled = True
+        self.d.cancel()
