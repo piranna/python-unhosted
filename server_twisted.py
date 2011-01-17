595c1843bd4c9e565c342ec0410ebe3405c1f261
piranna <piranna@localhost>
2011-01-17 23:20:13 +0100

    Basic UnHosted node and web server using Twisted and SQLite

---
+++ server_twisted.py
@@ -0,0 +1,29 @@
+import argparse
+import sqlite3
+
+from twisted.internet import reactor
+from twisted.web.server import Site
+from twisted.web.static import File
+
+from unhosted import Unhosted, storage,twisted,utils
+
+
+# Command line arguments
+parser = argparse.ArgumentParser(description='UnHosted node and webpages server.')
+parser.add_argument('--database', default=':memory:',
+                    help='database where to store UnHosted key-value pairs')
+parser.add_argument('--rootdir', default='.',
+                    help='root directory of the webserver')
+args = parser.parse_args()
+
+# Connect database and UnHosted interface
+db = storage.DatabaseStorage(sqlite3.connect(args.database))
+uh = Unhosted(db, utils.VoidChecker())
+
+# Serve webpages and UnHosted RPC
+root = File(args.rootdir)
+root.putChild("unhosted", tx.UnhostedResource(uh))
+
+# Start server
+reactor.listenTCP(8080, Site(root))
+reactor.run()
